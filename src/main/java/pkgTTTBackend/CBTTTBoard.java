5a4fc7dab6fefbf6fe334f8051967419e6069771
Caleb Brock <caleb.brock@broshin.com>
2025-01-29 17:32:22 -0800

    Use enum-based game state machine

--- src/main/java/pkgTTTBackend/CBTTTBoard.java
+++ src/main/java/pkgTTTBackend/CBTTTBoard.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class CBTTTBoard {
 
-	private enum State {
+	private enum CellState {
 		PLAYER, COMPUTER, EMPTY;
+
 		@Override
 		public String toString() {
 			return switch (this) {
@@ -16,12 +17,20 @@ public class CBTTTBoard {
 		}
 	}
 
-	private final State[][] board = new State[3][3];
+	private enum GameState {
+		PLAYER_TURN, COMPUTER_TURN, GAME_OVER
+	}
+
+	private final CellState[][] board = new CellState[3][3];
+
+	private GameState gameState = GameState.PLAYER_TURN;
+	private final Scanner scanner;
 
 	public CBTTTBoard() {
+		scanner = new Scanner(System.in);
 		for (int r = 0; r < 3; r++) {
 			for (int c = 0; c < 3; c++) {
-				board[r][c] = State.EMPTY;
+				board[r][c] = CellState.EMPTY;
 			}
 		}
 	}
@@ -36,7 +45,7 @@ public class CBTTTBoard {
 		int n = 0; // non-empty spaces
 		for (int r = 0; r < 3; r++) {
 			for (int c = 0; c < 3; c++) {
-				if (board[r][c] != State.EMPTY) {
+				if (board[r][c] != CellState.EMPTY) {
 					n++;
 				}
 			}
@@ -45,30 +54,51 @@ public class CBTTTBoard {
 	}
 
 	public void play() {
-		Scanner scanner = new Scanner(System.in);
+		while (gameState != GameState.GAME_OVER) {
+			switch (gameState) {
+				case PLAYER_TURN:
+					playPlayerTurn();
+					// todo: make computer turn once we create the computer turn probably in another assignment
+					// gameState = GameState.COMPUTER_TURN;
+					break;
+				case COMPUTER_TURN:
+					playComputerTurn();
+					gameState = GameState.PLAYER_TURN;
+					break;
+				default:
+					throw new IllegalStateException("Unexpected value: " + gameState);
+			}
+			// Post-turn, so printing the board
+			printBoard();
+			// End the game if the board is full
+			if (isFull()) {
+				gameState = GameState.GAME_OVER;
+			}
+		}
+	}
 
-		while (!isFull()) {
+	public void playPlayerTurn() {
 
-			int row = -1, col = -1;
-			while(row == -1 || col == -1 || board[row][col] != State.EMPTY) {
-				System.out.println("Enter the row and col for your entry - space (only) separated:");
-				row = scanner.nextInt();
-				col = scanner.nextInt();
+		int row = -1, col = -1;
+		while (row == -1 || col == -1 || board[row][col] != CellState.EMPTY) {
+			System.out.println("Enter the row and col for your entry - space (only) separated:");
+			row = scanner.nextInt();
+			col = scanner.nextInt();
 
-				if (row < 0 || row > 2 || col < 0 || col > 2) {
-					System.out.println("Enter a valid row and col for your entry - space (only) separated:");
-					continue;
-				}
-
-				if (board[row][col] != State.EMPTY) {
-					System.out.println("The cell is already marked. Please try again.");
-				}
+			if (row < 0 || row > 2 || col < 0 || col > 2) {
+				System.out.println("Enter a valid row and col for your entry - space (only) separated:");
+				continue;
 			}
 
-			board[row][col] = State.PLAYER;
-			printBoard();
+			if (board[row][col] != CellState.EMPTY) {
+				System.out.println("The cell is already marked. Please try again.");
+			}
 		}
-		scanner.close();
-		System.out.println("Game over!");
+
+		board[row][col] = CellState.PLAYER;
+	}
+
+	public void playComputerTurn() {
+
 	}
 }
